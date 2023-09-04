3fd00d77d4403f356adc92f2fa8031cef3b417ce
mskim0344 <mskim0344@gmail.com>
2023-09-04 18:10:54 +0900

    자바스크립트를 활용하여 상단 내비게이션 배경색 변화와

--- js/basic.js
+++ js/basic.js
@@ -1,10 +1,31 @@
-    var swiper = new Swiper(".mySwiper", {
-      direction: "vertical",
-      slidesPerView: "auto",
-      freeMode: true,
-      scrollbar: {
-        el: ".swiper-scrollbar",
-      },
-      mousewheel: true,
-    });
+    // var swiper = new Swiper(".mySwiper", {
+    //   direction: "vertical",
+    //   slidesPerView: "auto",
+    //   freeMode: true,
+    //   scrollbar: {
+    //     el: ".swiper-scrollbar",
+    //   },
+    //   mousewheel: true,
+    // });
   
+
+
+
+        // 내비게이션 바 요소 선택
+        const navbar = document.getElementById("navi");
+
+        // 스크롤 이벤트 리스너 등록
+        window.addEventListener("scroll", () => {
+            // 현재 스크롤 위치 확인
+            const scrollY = window.scrollY;
+
+            // 스크롤을 내릴 때, 배경색과 텍스트 색을 변경
+            if (scrollY > 0) {
+                navbar.style.backgroundColor = "white";
+                navbar.style.color = "black";
+            } else { // 스크롤을 올릴 때, 다시 초기 색상으로 변경
+                navbar.style.backgroundColor = "transparent";
+                navbar.style.color = "white";
+            }
+        });
+    
