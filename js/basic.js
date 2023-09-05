c3269c946563599c09c28ece2b26bf1b60e62aaa
mskim0344 <mskim0344@gmail.com>
2023-09-05 16:49:10 +0900

    스와이퍼 활용

--- js/basic.js
+++ js/basic.js
@@ -1,21 +1,17 @@
-        // // 내비게이션 바 요소 선택
-        // const navbar = document.getElementById("navi");
-        // const black = document.getElementsByClassName("black");
-        // const white = document.getElementsByClassName("white")
-
-        // // 스크롤 이벤트 리스너 등록
-        // window.addEventListener("scroll", () => {
-        //     // 현재 스크롤 위치 확인
-        //     const scrollY = window.scrollY;
-
-        //     // 스크롤을 내릴 때, 배경색과 텍스트 색을 변경
-        //     if (scrollY > 0) {
-        //         navbar.style.backgroundColor = "white";
-        //         removeEventListener(this, ()=>{
-                    
-        //         })
-        //     } else { // 스크롤을 올릴 때, 다시 초기 색상으로 변경
-        //         navbar.style.backgroundColor = "transparent";
-                
-        //     }
-        // });
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
