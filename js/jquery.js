d853ceb7b36cb90139b5069550b95a4194205cf1
mskim0344 <mskim0344@gmail.com>
2023-09-07 16:51:06 +0900

    애니메이션 효과 추가와 스크롤시 바디태그에 클래스 추가 퀵메뉴 생성 완료

--- js/jquery.js
+++ js/jquery.js
@@ -3,22 +3,24 @@ $(window).scroll(function() {
     var scrollY = $(window).scrollTop();
 
     // 스크롤을 내릴 때, 배경색과 텍스트 색을 변경
-    if (scrollY > 0) {
-        $("#navi").css({
-            "background-color": "white",
-            "color": "#111",
-            // opacity: "0.5"
-        });
-        $(".white").css({
-            "color": "#111"
-        });
+    if (scrollY > 100) {
+        $("body").addClass('scroll');
+        // $("#navi").css({
+        //     "background-color": "white",
+        //     "color": "#111",
+        //     // opacity: "0.5"
+        // });
+        // $(".white").css({
+        //     "color": "#111"
+        // });
     } else { // 스크롤을 올릴 때, 다시 초기 색상으로 변경
-        $("#navi").css({
-            "background-color": "transparent",
-            "color": "#efebe4"
-        });
-        $(".white").css({
-            "color": "#efebe4"
-        });
+        $("body").removeClass('scroll');
+        // $("#navi").css({
+        //     "background-color": "transparent",
+        //     "color": "#efebe4"
+        // });
+        // $(".white").css({
+        //     "color": "#efebe4"
+        // });
     }
 });
