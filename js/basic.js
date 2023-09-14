2ebadfa21c09146268b4f190ea1e3330b61e44d1
mskim0344 <mskim0344@gmail.com>
2023-09-14 16:44:45 +0900

    상단 반응형 수정과 #about 프로필 사진 들어가는 부분 영역 조절 스와이퍼 세팅 준비

--- js/basic.js
+++ js/basic.js
@@ -1,28 +1,11 @@
-// var swiper = new Swiper(".mySwiper", {
-//     effect: "coverflow",
-//     grabCursor: true,
-//     centeredSlides: true,
-//     loop: true,
-//     slidesPerView: "auto",
-//     coverflowEffect: {
-//       rotate: 50,
-//       stretch: 0,
-//       depth: 100,
-//       modifier: 1,
-//       slideShadows: true,
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//     },
-//   });
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  loop: true,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    autoHeight : true,
-    autowidth : true
-  },
-});
+    var swiper = new Swiper(".mySwiper", {
+      direction: "vertical",
+      slidesPerView: 1,
+      spaceBetween: 30,
+      mousewheel: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  
